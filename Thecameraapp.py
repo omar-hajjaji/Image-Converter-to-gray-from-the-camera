73390e325f28e0a10cdd4f549197edc174a93520
Hajjaji omar <79055225+omar-hajjaji@users.noreply.github.com>
2024-09-10 02:03:48 +0200

    Add files via upload

---
+++ Thecameraapp.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+
+# Start the camera:
+camera_image = st.camera_input("Camera")
+
+if camera_image:
+    # Create a pillow image instance:
+    img = Image.open(camera_image)
+
+    # Convert the pillow image to grayscale:
+    gray_img = img.convert("L")
+
+    # Render the grayscale image on the web page:
+    st.image(gray_img)
+
+
